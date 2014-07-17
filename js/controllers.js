77c6a116a26ae57c6d615dde8e76703eade3b4b2
tvler <tylerdeitz@gmail.com>
2014-07-16 22:26:58 -0700

    updating execs

--- js/controllers.js
+++ js/controllers.js
@@ -23,7 +23,7 @@ angular.module('controllers', [])
    $scope.people = [
       {
          'title': 'General Manager',
-         'name': 'Tyler Deitz',
+         'name': 'Parker Glenn',
          'description' : 'Our General Manager oversee all other KCPR executives, DJs, and functions. They are often our liasons with entities outside of KCPR. If you have a question about KCPR that doesn\'t fit with any of the other executes, shoot them an email.',
          'email': 'generalmanagers.kcpr@gmail.com'
       },
