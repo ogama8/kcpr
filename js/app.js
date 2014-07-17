c70954377e2be1052620cf7f979a29d4ef3ae4d5
tvler <tylerdeitz@gmail.com>
2014-07-16 21:14:04 -0700

    experimenting with OPERATION: sexy urls (may not work though

--- js/app.js
+++ js/app.js
@@ -2,43 +2,44 @@ var gdocs = 'https://spreadsheets.google.com/feeds/list/0Ao8Bu17C9mFydFUyWDJXaF9
 
 angular.module('kcpr-app', ['ngRoute', 'controllers', 'loadscript'])
 
-.config(function($routeProvider) {
+.config(function($routeProvider, $locationProvider) {
 
 
-   $routeProvider.when("/news", {
-      templateUrl: 'partials/news.html'
-   });
+   $routeProvider
+      .when("/news", {
+         templateUrl: 'partials/news.html'
+      })
 
-   $routeProvider.when("/schedule", {
-      templateUrl: 'partials/schedule.html',
-      controller: 'ScheduleCtrl',
-      resolve: {
-         schedule: ['$http', function($http) {
-            return $http.get(gdocs)
-               .then(function (data) {
-                  return data.data;
-               });
-         }]
-      }
-   });
+      .when("/schedule", {
+         templateUrl: 'partials/schedule.html',
+         controller: 'ScheduleCtrl',
+         resolve: {
+            schedule: ['$http', function($http) {
+               return $http.get(gdocs)
+                  .then(function (data) {
+                     return data.data;
+                  });
+            }]
+         }
+      })
+      .when("/join", {
+         templateUrl: 'partials/join.html'
+      })
+      .when("/contact", {
+         templateUrl: 'partials/contact.html',
+         controller: 'ContactCtrl'
+      })
+      .when("/listen", {
+         templateUrl: 'partials/listen.html'
+      })
+      .when("/about", {
+         templateUrl: 'partials/about.html'
+      })
+      .when("/pledge", {
+         templateUrl: 'partials/pledge.html'
+      })
+      .otherwise({ redirectTo: "/news" });
 
-   $routeProvider.when("/join", {
-      templateUrl: 'partials/join.html'
-   });
-   $routeProvider.when("/contact", {
-      templateUrl: 'partials/contact.html',
-      controller: 'ContactCtrl'
-   });
-   $routeProvider.when("/listen", {
-      templateUrl: 'partials/listen.html'
-   });
-   $routeProvider.when("/about", {
-      templateUrl: 'partials/about.html'
-   });
-   $routeProvider.when("/pledge", {
-      templateUrl: 'partials/pledge.html'
-   });
+   $locationProvider.html5Mode(true);
 
-   $routeProvider.otherwise({ redirectTo: "/news" });
-
-})
+});
